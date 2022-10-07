4b9658232905d57ea9fe9f852d831441b2ffacfa
Sung-- <vitorhugomota99@hotmail.com>
2022-10-07 09:48:16 -0300

    Decimal to binary

---
+++ DecToBin.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void dec2bin(int dec)
+{
+if(dec >= 1){
+ 
+  if(dec % 2 == 1){
+        
+	dec = dec/2;
+	dec2bin(dec);
+	printf("1");
+	
+	
+	
+   }else{
+         
+	 dec = dec/2;
+         dec2bin(dec);
+         printf("0");
+         
+   }
+
+}
+}
+
+
+
+int main()
+{
+int num;
+   
+   	printf("Digite um numero inteiro: ");
+        scanf("%d", &num);
+	printf("Resultado: ");
+	if(num == 0)printf("0");
+        else{ 
+		dec2bin(num);
+	}
+	printf("\n");
+	
+}
