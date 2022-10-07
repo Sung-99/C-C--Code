100749910623c1f61c69eeef818154ab90866104
Sung-- <vitorhugomota99@hotmail.com>
2022-10-07 09:53:28 -0300

    Find the better box game

---
+++ BoxGame.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+int main()
+{
+    FILE *fp;
+    char str[30];
+    int x[80][80], limite,soma,posx,posy, maxx=0;
+    printf("Digite o nome do arquivo de entrada: ");
+    scanf("%s",str);
+    if (!(fp = fopen(str,"r"))){ //Caso ocorra algum erro na abertura do arquivo
+        printf("Erro! Impossivel abrir o arquivo!\n");
+        return 0;
+    }
+
+
+    // Imprime conteudo do arquivo
+    fscanf(fp,"%d",&limite);
+    for(int l = 1; l <= limite; l++){
+       for(int c = 1; c < l + 1; c++){
+            fscanf(fp,"%i",&x[l][c]);
+       }
+    }
+
+    maxx = x[1][1];
+    if(limite == 1){
+                maxx = x[1][1];
+                posx =1;
+                posy =1;
+    }else{
+        for(int l = 1; l <= limite; l++){
+
+            for(int c = 1; c < l + 1; c++){
+
+                for(int l2 = 1; l2 < l; l2++){
+                    for(int c2 = 1; c2 < l2 + 1 ; c2++){
+                        if(l2 == 1 && c2 == 1){
+                         soma = x[l][c] + x[l2][c2];
+                        }else{
+                         soma = soma + x[l2][c2];
+                        }
+
+                    }
+
+                }
+                if(l>1){
+                    if(soma > maxx){
+                                maxx =soma;
+                                posx =l;
+                                posy =c;
+                    }
+                }
+
+            }
+        }
+
+    }
+    printf("Resposta: fileira %i, caixa %i.",posx,posy);
+
+    fclose(fp);
+    return 0;
+}
