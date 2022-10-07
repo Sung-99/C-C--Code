6e2d1ee604a14aec0a85410f9c2ff5cdbce12c80
Sung-- <vitorhugomota99@hotmail.com>
2022-10-07 07:57:47 -0300

    Mdc with recursion exercise

---
+++ Mdc.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int mdc(int a, int b){
+ if(a > b){
+	if(b == 1){
+	    if(a %)
+	    return mdc(a,b);
+	}
+ }
+
+}
+
+int main() {
+
+	int a, b, resultado;
+	
+	printf("Digite 2 numeros inteiros: ");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	
+	resultado = mdc(a, b);
+	
+	
+	printf("Resultado: %d\n", resultado);
+	return 0;
+}
